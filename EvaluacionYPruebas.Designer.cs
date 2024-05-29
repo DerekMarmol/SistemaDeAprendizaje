d7ddfb2f471bdfc63f2696a43a766c2b94fdfb7c
Daniel Alexander Ortiz Cabrera <dortizc13@miumg.edu.gt>
2024-05-29 15:56:23 -0600

    Form Evaluaciones y Pruebas

---
+++ EvaluacionYPruebas.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SistemaDeAprendizaje
+{
+    partial class EvaluacionYPruebas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EvaluacionYPruebas));
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnSiguiente.FlatAppearance.BorderSize = 0;
+            this.btnSiguiente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSiguiente.Location = new System.Drawing.Point(358, 273);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(75, 23);
+            this.btnSiguiente.TabIndex = 7;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = false;
+            this.btnSiguiente.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.AutoSize = true;
+            this.lblPregunta.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblPregunta.Location = new System.Drawing.Point(330, 86);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Size = new System.Drawing.Size(53, 13);
+            this.lblPregunta.TabIndex = 5;
+            this.lblPregunta.Text = "Pregunta:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lblResultado.Location = new System.Drawing.Point(39, 30);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnEnviar.FlatAppearance.BorderSize = 0;
+            this.btnEnviar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEnviar.Location = new System.Drawing.Point(358, 217);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 9;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.txtRespuesta.Location = new System.Drawing.Point(177, 154);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(461, 20);
+            this.txtRespuesta.TabIndex = 8;
+            this.txtRespuesta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 10;
+            // 
+            // EvaluacionYPruebas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.lblPregunta);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "EvaluacionYPruebas";
+            this.Text = "EvaluacionYPruebas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Label lblPregunta;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtRespuesta;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ EvaluacionYPruebas.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeAprendizaje
+{
+    public partial class EvaluacionYPruebas : Form
+
+    {
+        private List<Pregunta> preguntas;
+        private int preguntaActual;
+        private int respuestasCorrectas;
+        public EvaluacionYPruebas()
+
+        {
+            InitializeComponent();
+            CargarPreguntas();
+            MostrarPregunta();
+
+        }
+        private void CargarPreguntas()
+        {
+            preguntas = new List<Pregunta>
+            {
+                new Pregunta { Texto = "¿Cuál es la capital de Guatemala?", RespuestaCorrecta = "Ciudad de Guatemala" },
+                new Pregunta { Texto = "¿Cuánto es 2 + 2?", RespuestaCorrecta = "4" },
+                new Pregunta { Texto = "¿Cuál es el color del cielo?", RespuestaCorrecta = "Azul" }
+            };
+            preguntaActual = 0;
+        }
+
+        private void MostrarPregunta()
+        {
+            if (preguntaActual < preguntas.Count)
+            {
+                lblPregunta.Text = preguntas[preguntaActual].Texto;
+                txtRespuesta.Clear();
+                lblResultado.Text = "";
+                btnEnviar.Enabled = true;
+                btnSiguiente.Visible = false;
+                txtRespuesta.Enabled = true;
+                txtRespuesta.Focus();
+            }
+            else
+            {
+                lblPregunta.Text = "¡Examen terminado!";
+                txtRespuesta.Enabled = false;
+                btnEnviar.Enabled = false;
+                btnSiguiente.Enabled = false;
+                lblResultado.Text = "";
+                MostrarPorcentaje();
+            }
+        }
+        private void MostrarPorcentaje()
+        {
+            double porcentaje = ((double)respuestasCorrectas / preguntas.Count) * 100;
+            lblResultado.Text = $"Tu porcentaje de respuestas correctas es: {porcentaje}%";
+            lblResultado.ForeColor = System.Drawing.Color.Black;
+        }
+
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            string respuestaUsuario = txtRespuesta.Text.Trim();
+            if (respuestaUsuario.Equals(preguntas[preguntaActual].RespuestaCorrecta, StringComparison.OrdinalIgnoreCase))
+            {
+                lblResultado.Text = "Correcto!";
+                lblResultado.ForeColor = System.Drawing.Color.Green;
+                respuestasCorrectas++;
+            }
+            else
+            {
+                lblResultado.Text = "Incorrecto!";
+                lblResultado.ForeColor = System.Drawing.Color.Red;
+            }
+            btnEnviar.Enabled = false;
+            btnSiguiente.Visible = true;
+            txtRespuesta.Enabled = false;
+
+        }
+
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            preguntaActual++;
+            MostrarPregunta();
+        }
+        public class Pregunta
+        {
+            public string Texto { get; set; }
+            public string RespuestaCorrecta { get; set; }
+        }
+
+    }
+}
