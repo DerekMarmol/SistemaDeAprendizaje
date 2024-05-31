3db95df77211a4ebb5fad826d10e871166157cc6
aulloa99 <aulloat@miumg.edu.gt>
2024-05-30 22:35:20 -0600

    Agregar formulario Calificaciones

---
+++ FormCalifYRetro.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace SistemaDeAprendizaje
+{
+    partial class FormCalifYRetro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbEstudiante = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.cmbCursos = new System.Windows.Forms.ComboBox();
+            this.txtCalificacion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRetroalimentacion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dvgCalificaciones = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgCalificaciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Estudiante";
+            // 
+            // cmbEstudiante
+            // 
+            this.cmbEstudiante.AccessibleDescription = "comboBoxEstudiantes";
+            this.cmbEstudiante.AccessibleName = "Estudiantes";
+            this.cmbEstudiante.FormattingEnabled = true;
+            this.cmbEstudiante.ItemHeight = 13;
+            this.cmbEstudiante.Location = new System.Drawing.Point(229, 61);
+            this.cmbEstudiante.Name = "cmbEstudiante";
+            this.cmbEstudiante.Size = new System.Drawing.Size(276, 21);
+            this.cmbEstudiante.TabIndex = 17;
+            this.cmbEstudiante.SelectedIndexChanged += new System.EventHandler(this.cmbEstudiante_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 21F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Indigo;
+            this.label5.Location = new System.Drawing.Point(40, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(493, 37);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Calificaciones y Retroalimentación";
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(607, 414);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(121, 24);
+            this.btnRegresar.TabIndex = 27;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(436, 415);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(121, 24);
+            this.btnEditar.TabIndex = 26;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(260, 414);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(128, 26);
+            this.btnGuardar.TabIndex = 25;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // cmbCursos
+            // 
+            this.cmbCursos.AccessibleDescription = "comboBoxCursos";
+            this.cmbCursos.AccessibleName = "Cursos ";
+            this.cmbCursos.FormattingEnabled = true;
+            this.cmbCursos.Location = new System.Drawing.Point(666, 25);
+            this.cmbCursos.Name = "cmbCursos";
+            this.cmbCursos.Size = new System.Drawing.Size(276, 21);
+            this.cmbCursos.TabIndex = 18;
+            // 
+            // txtCalificacion
+            // 
+            this.txtCalificacion.Location = new System.Drawing.Point(667, 62);
+            this.txtCalificacion.Name = "txtCalificacion";
+            this.txtCalificacion.Size = new System.Drawing.Size(55, 20);
+            this.txtCalificacion.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(573, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Curso";
+            // 
+            // txtRetroalimentacion
+            // 
+            this.txtRetroalimentacion.Location = new System.Drawing.Point(668, 113);
+            this.txtRetroalimentacion.Multiline = true;
+            this.txtRetroalimentacion.Name = "txtRetroalimentacion";
+            this.txtRetroalimentacion.Size = new System.Drawing.Size(274, 71);
+            this.txtRetroalimentacion.TabIndex = 22;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(573, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Calificacion ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(573, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(92, 13);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Retroalimentacion";
+            // 
+            // dvgCalificaciones
+            // 
+            this.dvgCalificaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgCalificaciones.Location = new System.Drawing.Point(229, 191);
+            this.dvgCalificaciones.Name = "dvgCalificaciones";
+            this.dvgCalificaciones.Size = new System.Drawing.Size(421, 150);
+            this.dvgCalificaciones.TabIndex = 28;
+            // 
+            // FormCalifYRetro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dvgCalificaciones);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRetroalimentacion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCalificacion);
+            this.Controls.Add(this.cmbCursos);
+            this.Controls.Add(this.cmbEstudiante);
+            this.Controls.Add(this.label5);
+            this.Name = "FormCalifYRetro";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dvgCalificaciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbEstudiante;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ComboBox cmbCursos;
+        private System.Windows.Forms.TextBox txtCalificacion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRetroalimentacion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.DataGridView dvgCalificaciones;
+    }
+}
---
+++ FormCalifYRetro.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SistemaDeAprendizaje
+{
+    public partial class FormCalifYRetro : Form
+    {
+        private string connectionString = "Server=bofn3obbnejxfyoheir1-mysql.services.clever-cloud.com;Database=bofn3obbnejxfyoheir1;User=uh4dunztmvwgo47z;Password=uyjiJZkG5JqLtaELmvku;Port=3306;SslMode=Preferred;";
+
+        public FormCalifYRetro()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            CargarEstudiantes();
+            CargarCursos();
+            if (cmbEstudiante.SelectedItem != null)
+            {
+                int idEstudiante = ObtenerIdSeleccionado(cmbEstudiante.SelectedItem.ToString());
+                MessageBox.Show("seleccionado " + idEstudiante);
+                CargarCalificacionesEstudiante(idEstudiante);
+            }
+            else
+            {
+                MessageBox.Show("no hay seleccionado ");
+            }
+        }
+
+
+        private void cmbEstudiante_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Evento cmbEstudiante_SelectedIndexChanged disparado.");
+            if (cmbEstudiante.SelectedItem != null)
+            {
+                int idEstudiante = ObtenerIdSeleccionado(cmbEstudiante.SelectedItem.ToString());
+                CargarCalificacionesEstudiante(idEstudiante);
+            }
+        }
+
+        private void CargarCalificacionesEstudiante(int idEstudiante)
+        {
+            MessageBox.Show("CargarCalificacionesEstudiante llamada con el ID de estudiante: " + idEstudiante);
+            DataTable dtCalificaciones = new DataTable();
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT CalificacionID, CursoID, Calificacion, Retroalimentacion FROM Calificaciones WHERE EstudianteID = @EstudianteID";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@EstudianteID", idEstudiante);
+                    try
+                    {
+                        connection.Open();
+                        using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                        {
+                            adapter.Fill(dtCalificaciones);
+                            dvgCalificaciones.DataSource = dtCalificaciones;
+                        }
+                        MessageBox.Show("se cargó la data del grid ");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al cargar las calificaciones del estudiante: " + ex.Message);
+                    }
+                    finally
+                    {
+                        if (connection.State == ConnectionState.Open)
+                            connection.Close();
+                    }
+                }
+            }
+
+            dvgCalificaciones.DataSource = dtCalificaciones;
+        }
+
+
+        private void CargarEstudiantes()
+        {
+            List<string> estudiantes = new List<string>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT UsuarioID, Nombre FROM Usuarios as Estudiantes";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                estudiantes.Add($"{reader.GetInt32("UsuarioID")} - {reader.GetString("Nombre")}");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al cargar los estudiantes: " + ex.Message);
+                    }
+                    finally
+                    {
+                        if (connection.State == ConnectionState.Open)
+                            connection.Close();
+                    }
+                }
+            }
+
+            cmbEstudiante.DataSource = estudiantes;
+        }
+
+        private void CargarCursos()
+        {
+            List<string> cursos = new List<string>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT CursoID, NombreCurso FROM Cursos";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                cursos.Add($"{reader.GetInt32("CursoID")} - {reader.GetString("NombreCurso")}");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al cargar los cursos: " + ex.Message);
+                    }
+                    finally
+                    {
+                        if (connection.State == ConnectionState.Open)
+                            connection.Close();
+                    }
+                }
+            }
+
+            cmbCursos.DataSource = cursos;
+        }
+
+        private int ObtenerIdSeleccionado(string seleccionado)
+        {
+            int idSelected = int.Parse(seleccionado.Split('-')[0].Trim());
+            MessageBox.Show("Evento cmbEstudiante_SelectedIndexChanged disparado. " + idSelected);
+            return idSelected;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (cmbEstudiante.SelectedItem == null || cmbCursos.SelectedItem == null || string.IsNullOrEmpty(txtCalificacion.Text) || string.IsNullOrEmpty(txtRetroalimentacion.Text))
+            {
+                MessageBox.Show("Por favor, complete todos los campos.");
+                return;
+            }
+
+            int idEstudiante = ObtenerIdSeleccionado(cmbEstudiante.SelectedItem.ToString());
+            MessageBox.Show("ObtenerIdSeleccionado "+ idEstudiante );
+            int idCurso = ObtenerIdSeleccionado(cmbCursos.SelectedItem.ToString());
+            int calificacion;
+
+            if (!int.TryParse(txtCalificacion.Text, out calificacion))
+            {
+                MessageBox.Show("Por favor, ingrese una calificación válida.");
+                return;
+            }
+
+            string retroalimentacion = txtRetroalimentacion.Text;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT CalificacionID FROM Calificaciones WHERE EstudianteID = @IdEstudiante AND CursoID = @CursoID";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@IdEstudiante", idEstudiante);
+                    command.Parameters.AddWithValue("@CursoID", idCurso);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+
+                        if (result != null)
+                        {
+                            // Existe, actualizar
+                            int calificacionID = Convert.ToInt32(result);
+                            string updateQuery = "UPDATE Calificaciones SET Calificacion = @Calificacion, Retroalimentacion = @Retroalimentacion WHERE CalificacionID = @CalificacionID";
+                            using (MySqlCommand updateCommand = new MySqlCommand(updateQuery, connection))
+                            {
+                                updateCommand.Parameters.AddWithValue("@Calificacion", calificacion);
+                                updateCommand.Parameters.AddWithValue("@Retroalimentacion", retroalimentacion);
+                                updateCommand.Parameters.AddWithValue("@CalificacionID", calificacionID);
+
+                                int updateResult = updateCommand.ExecuteNonQuery();
+                                if (updateResult > 0)
+                                {
+                                    MessageBox.Show("La calificación y retroalimentación se han actualizado correctamente.");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Error al actualizar la calificación y retroalimentación.");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            // No existe, insertar
+                            string insertQuery = "INSERT INTO Calificaciones (EstudianteID, CursoID, Calificacion, Retroalimentacion) VALUES (@IdEstudiante, @CursoID, @Calificacion, @Retroalimentacion)";
+                            using (MySqlCommand insertCommand = new MySqlCommand(insertQuery, connection))
+                            {
+                                insertCommand.Parameters.AddWithValue("@IdEstudiante", idEstudiante);
+                                insertCommand.Parameters.AddWithValue("@CursoID", idCurso);
+                                insertCommand.Parameters.AddWithValue("@Calificacion", calificacion);
+                                insertCommand.Parameters.AddWithValue("@Retroalimentacion", retroalimentacion);
+
+                                int insertResult = insertCommand.ExecuteNonQuery();
+                                if (insertResult > 0)
+                                {
+                                    MessageBox.Show("La calificación y retroalimentación se han guardado correctamente.");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Error al guardar la calificación y retroalimentación.");
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error al guardar la calificación y retroalimentación: " + ex.Message);
+                    }
+                    finally
+                    {
+                        if (connection.State == ConnectionState.Open)
+                            connection.Close();
+                    }
+                }
+            }
+        }
+    }
+}
